ee7cce21c4d21dff6840a308686c04afda375738
kimss018 <kimss018@naver.com>
2022-06-13 12:09:53 +0900

    수업예제

---
+++ Ch04/4_MethodStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 : 2022/06/07
+ * 이름 : 김효경
+ * 내용 : 메서드 실습하기 교재 p265
+ */
+namespace Ch04
+{
+    internal class _4_MethodStack
+    {
+        static void Main1(string[] args)
+        {
+            int result = 0;
+            int n1 = 1;
+            int n2 = 2; 
+
+            result = Sum(n1, n2);
+
+            Console.WriteLine("result : " + result);
+        }
+
+        public static int Sum(int start, int end)
+        {
+            int total = 0;
+
+            for (int k = start; k <= end; k++)
+            {
+                total += k;
+            }
+
+            return total;
+        }
+    }
+}
