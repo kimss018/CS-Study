41f4e24d9544c9f45d075b138f61252762696dd6
kimss018 <kimss018@naver.com>
2022-06-16 14:31:45 +0900

    연습문제

---
+++ Test/2/2_04.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2
+{
+    internal class _2_04
+    {
+        static void Main1(string[] args)
+        {
+            int[] arr = { 4, 2, 1, 5, 3 };
+            
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = i + 1; j < 5; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            foreach ( int n in arr)
+            {
+                Console.Write(n + " ");
+            }
+        }
+    }
+}
