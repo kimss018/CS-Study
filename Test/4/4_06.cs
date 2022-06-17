3aa776fc1a66c37554205b87a0deb35e72c2f2a5
kimss018 <kimss018@naver.com>
2022-06-17 12:14:41 +0900

    연습문제

---
+++ Test/4/4_06.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._4
+{
+   class MyIndexer<T>
+    {
+        private T[] array = new T[10];
+
+        public T this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = value; }
+        }
+    }
+
+    internal class _4_06
+    {
+        static void Main1(string[] args)
+        {
+            var myIdexer = new MyIndexer<string>();
+
+            myIdexer[0] = "Hello, World!";
+            myIdexer[1] = "Hello, Korea!";
+            myIdexer[2] = "Hello, Busan!";
+            myIdexer[3] = "Hello, C#!";
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(myIdexer[i]);
+            }
+            
+        }
+    }
+}
