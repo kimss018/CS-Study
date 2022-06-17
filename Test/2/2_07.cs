fa90c7d142023a83da26c3930ca9f24b5555060f
kimss018 <kimss018@naver.com>
2022-06-17 17:16:17 +0900

    연습문제

---
+++ Test/2/2_07.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test._2
+{
+    internal class _2_07
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("1과 5의 최대공약수 : " + Gcd(1, 5));
+            Console.WriteLine("1과 5의 최대공약수 : " + Gcd(3, 6));
+            Console.WriteLine("1과 5의 최대공약수 : " + Gcd(12, 18));
+            Console.WriteLine("1과 5의 최대공약수 : " + Gcd(60, 24));
+            Console.WriteLine("1과 5의 최대공약수 : " + Gcd(192, 162));
+
+        }
+
+        public static int Gcd(int a, int b)
+        {
+            int temp;
+
+            if (a < b)
+                temp = a;
+            else
+                temp = b;
+            while (true)
+            {
+                if (a % temp == 0 && b % temp == 0)
+                {
+                    break;
+                  
+                }
+                temp--;
+            }
+            return temp;
+
+        }
+    }
+}
