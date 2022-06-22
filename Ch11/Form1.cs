865113e1a99e1469a6911fd605fac82d649293fe
kimss018 <kimss018@naver.com>
2022-06-22 12:34:11 +0900

    2022/06/22 수업예제

---
+++ Ch11/Form1.cs
@@ -0,0 +1,103 @@
+namespace Ch11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("메세지박스1");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("제목과 내용이 있는 메세지박스", "메세지박스2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("두개의 버튼을 갖는 메세지박스", "메세지 박스3", MessageBoxButtons.YesNo);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("세개의 버튼을 갖는 메세지박스", "메세지박스4", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Exclamation);
+        }
+
+        private void btnUid_Click(object sender, EventArgs e)
+        {
+
+            string uid =  txtUid.Text;
+            lbUid.Text = "결과 : " + uid;
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            lbName.Text = "결과 : " + lbUid;
+        }
+
+        private void btnHp_Click(object sender, EventArgs e)
+        {
+            lbHp.Text = "결과 : " + txtHp.Text;
+        }
+
+        private void btnFruit_Click(object sender, EventArgs e)
+        {
+            CheckBox[] chbFruits = { chbFruit1, chbFruit2, chbFruit3, chbFruit4, chbFruit5 };
+
+            List<string> fruits = new List<string>();
+
+            foreach (CheckBox chb in chbFruits)
+            {
+                if (chb.Checked)
+                {
+                    fruits.Add(chb.Text);
+                }
+            }
+
+            lbResultFruit.Text = "선택한 과일 : " + String.Join(", ", fruits);
+        }
+
+        private void btnGender_Click(object sender, EventArgs e)
+        {
+            if ( rdMale.Checked)
+            {
+                lbResultGender.Text = "결과 : 남자";
+            }
+            else
+            {
+                lbResultGender.Text = "결과 : 여자";
+            }
+        }
+
+        private void btnYear_Click(object sender, EventArgs e)
+        {
+            if( rdYear1.Checked)
+            {
+                lbResultYear.Text = "결과 : 1학년";
+            }
+            else if (rdYear2.Checked)
+            {
+                lbResultYear.Text = "결과 : 2학년";
+            }
+            else if (rdYear3.Checked)
+            {
+                lbResultYear.Text = "결과 : 3학년";
+            }
+            else if (rdYear4.Checked)
+            {
+                lbResultYear.Text = "결과 : 4학년";
+            }
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ Ch11/Form2.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Ch11
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbPosition = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbResultPosition = new System.Windows.Forms.Label();
+            this.btnPosition = new System.Windows.Forms.Button();
+            this.btnHobby = new System.Windows.Forms.Button();
+            this.lbResultHobby = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbHobby = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstcountry = new System.Windows.Forms.ListBox();
+            this.lbResultCountry = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvMember = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMember)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "데이터를 공급해주는 컨트롤";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ComboBox 연습";
+            // 
+            // cbPosition
+            // 
+            this.cbPosition.FormattingEnabled = true;
+            this.cbPosition.Location = new System.Drawing.Point(49, 85);
+            this.cbPosition.Name = "cbPosition";
+            this.cbPosition.Size = new System.Drawing.Size(121, 23);
+            this.cbPosition.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "직급";
+            // 
+            // lbResultPosition
+            // 
+            this.lbResultPosition.AutoSize = true;
+            this.lbResultPosition.Location = new System.Drawing.Point(257, 88);
+            this.lbResultPosition.Name = "lbResultPosition";
+            this.lbResultPosition.Size = new System.Drawing.Size(42, 15);
+            this.lbResultPosition.TabIndex = 4;
+            this.lbResultPosition.Text = "결과 : ";
+            // 
+            // btnPosition
+            // 
+            this.btnPosition.Location = new System.Drawing.Point(176, 84);
+            this.btnPosition.Name = "btnPosition";
+            this.btnPosition.Size = new System.Drawing.Size(75, 23);
+            this.btnPosition.TabIndex = 5;
+            this.btnPosition.Text = "확인";
+            this.btnPosition.UseVisualStyleBackColor = true;
+            this.btnPosition.Click += new System.EventHandler(this.btnPosition_Click);
+            // 
+            // btnHobby
+            // 
+            this.btnHobby.Location = new System.Drawing.Point(176, 113);
+            this.btnHobby.Name = "btnHobby";
+            this.btnHobby.Size = new System.Drawing.Size(75, 23);
+            this.btnHobby.TabIndex = 9;
+            this.btnHobby.Text = "확인";
+            this.btnHobby.UseVisualStyleBackColor = true;
+            this.btnHobby.Click += new System.EventHandler(this.btnHobby_Click);
+            // 
+            // lbResultHobby
+            // 
+            this.lbResultHobby.AutoSize = true;
+            this.lbResultHobby.Location = new System.Drawing.Point(257, 117);
+            this.lbResultHobby.Name = "lbResultHobby";
+            this.lbResultHobby.Size = new System.Drawing.Size(42, 15);
+            this.lbResultHobby.TabIndex = 8;
+            this.lbResultHobby.Text = "결과 : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "취미";
+            // 
+            // cbHobby
+            // 
+            this.cbHobby.FormattingEnabled = true;
+            this.cbHobby.Location = new System.Drawing.Point(49, 114);
+            this.cbHobby.Name = "cbHobby";
+            this.cbHobby.Size = new System.Drawing.Size(121, 23);
+            this.cbHobby.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ListBox 연습";
+            // 
+            // lstcountry
+            // 
+            this.lstcountry.FormattingEnabled = true;
+            this.lstcountry.ItemHeight = 15;
+            this.lstcountry.Location = new System.Drawing.Point(12, 181);
+            this.lstcountry.Name = "lstcountry";
+            this.lstcountry.Size = new System.Drawing.Size(205, 94);
+            this.lstcountry.TabIndex = 11;
+            this.lstcountry.SelectedIndexChanged += new System.EventHandler(this.lstcountry_SelectedIndexChanged);
+            // 
+            // lbResultCountry
+            // 
+            this.lbResultCountry.AutoSize = true;
+            this.lbResultCountry.Location = new System.Drawing.Point(223, 260);
+            this.lbResultCountry.Name = "lbResultCountry";
+            this.lbResultCountry.Size = new System.Drawing.Size(42, 15);
+            this.lbResultCountry.TabIndex = 12;
+            this.lbResultCountry.Text = "결과 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "DataGridView 연습";
+            // 
+            // dgvMember
+            // 
+            this.dgvMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMember.Location = new System.Drawing.Point(11, 321);
+            this.dgvMember.Name = "dgvMember";
+            this.dgvMember.Size = new System.Drawing.Size(240, 150);
+            this.dgvMember.TabIndex = 14;
+            this.dgvMember.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 588);
+            this.Controls.Add(this.dgvMember);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbResultCountry);
+            this.Controls.Add(this.lstcountry);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnHobby);
+            this.Controls.Add(this.lbResultHobby);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbHobby);
+            this.Controls.Add(this.btnPosition);
+            this.Controls.Add(this.lbResultPosition);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbPosition);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMember)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox cbPosition;
+        private Label label3;
+        private Label lbResultPosition;
+        private Button btnPosition;
+        private Button btnHobby;
+        private Label lbResultHobby;
+        private Label label6;
+        private ComboBox cbHobby;
+        private Label label4;
+        private ListBox lstcountry;
+        private Label lbResultCountry;
+        private Label label5;
+        private DataGridView dgvMember;
+    }
+}
