ffe17e2311634368406deb379df83b10e4dcc59f
kimss018 <kimss018@naver.com>
2022-06-28 17:16:40 +0900

    프로젝트

---
+++ Project2/DBAccess.cs
@@ -0,0 +1,197 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class DBAccess
+    {
+        // 데이터베이스 접속 정보
+        private const string SERVER = "127.0.0.1";
+        private const string PORT = "3306";
+        private const string USERNAME = "root";
+        private const string PASSWORD = "1234";
+        private const string DATABASE = "mydb";
+        private const string TABLE = "user2";
+
+        // 싱글톤
+        private static DBAccess instance = new DBAccess();
+        public static DBAccess Instance { get { return instance; } }
+        private DBAccess() { }
+
+        // 데이터베이스 커넥션 생성 메서드
+        public MySqlConnection Connect()
+        {
+            string strConn = $"server = {SERVER}; port = {PORT}; username = {USERNAME}; password = {PASSWORD}; database = {DATABASE};";
+            MySqlConnection conn = new MySqlConnection(strConn);
+            return conn;
+        }
+
+
+        public void InsertUser(string uid, string name, string hp, decimal age)
+        {
+            MySqlConnection conn = Connect();
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = $"INSERT INTO `{TABLE}` VALUES ('{uid}', '{name}', '{hp}', {age})";
+                cmd.ExecuteNonQuery();
+            }
+
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            MessageBox.Show("데이터가 저장되었습니다.", "Insert 완료");
+        }
+
+        public void SelectUser(string uid, string name, string hp, decimal age) 
+        {
+            MySqlConnection conn = Connect();
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = $"SELECT INTO `user2` VALUES ('{uid}', '{name}', '{hp}', {age})";
+                cmd.ExecuteNonQuery();
+            }
+
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            MessageBox.Show("데이터가 저장되었습니다.", "Insert 완료");
+        }
+
+        public List<User> SelectUsers()
+        {
+            // 데이터 공급 리스트 생성
+            List<User> users = new List<User>();
+
+            MySqlConnection conn = Connect();
+
+            try
+            {
+                // 데이터베이스 접속
+                conn.Open();
+
+                // SQL 실행
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM `user2`";
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                // 결과 리스트 생성
+                while (reader.Read())
+                {
+                    User user = new User();
+                    user.Uid = reader[0].ToString();
+                    user.Name = reader[1].ToString();
+                    user.Hp = reader[2].ToString();
+                    user.Age = Convert.ToInt32(reader[3]);
+
+                    users.Add(user);
+                }
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message);
+            }
+            finally
+            {
+                // 데이터베이스 종료
+                conn.Close();
+            }
+
+            return users;
+        }
+
+        public void UpdateUser(string uid, string name, string hp, decimal age)
+        {
+            // 데이터 공급 리스트 생성
+            List<User> users = new List<User>();
+
+            MySqlConnection conn = Connect();
+
+            try
+            {
+                // 데이터베이스 접속
+                conn.Open();
+
+                // SQL 실행
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = $"UPDATE {TABLE} SET `name`='{name}', `hp`='{hp}', `age`={age} WHERE `uid`='{uid}'";
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                // 결과 리스트 생성
+                while (reader.Read())
+                {
+                    User user = new User();
+                    user.Uid = reader[0].ToString();
+                    user.Name = reader[1].ToString();
+                    user.Hp = reader[2].ToString();
+                    user.Age = Convert.ToInt32(reader[3]);
+
+                    users.Add(user);
+                }
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message);
+            }
+            finally
+            {
+                // 데이터베이스 종료
+                conn.Close();
+            }
+
+             MessageBox.Show("데이터가 수정되었습니다.", "Update 완료");
+        }
+
+        public void DeleteUser(string uid) 
+        {
+            // 데이터 공급 리스트 생성
+            List<User> users = new List<User>();
+
+            MySqlConnection conn = Connect();
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = $"DELETE FROM {TABLE} WHERE `uid`='{uid}'";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine(except.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            MessageBox.Show("데이터가 삭제되었습니다.", "Delete 완료");
+        }
+
+
+    }
+}
+
---
+++ Project2/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUid = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.nAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(482, 426);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "데이터 추가/수정/삭제";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "아이디";
+            // 
+            // txtUid
+            // 
+            this.txtUid.Location = new System.Drawing.Point(557, 87);
+            this.txtUid.Name = "txtUid";
+            this.txtUid.Size = new System.Drawing.Size(151, 23);
+            this.txtUid.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(557, 116);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 23);
+            this.txtName.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(500, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "이름";
+            // 
+            // txtHp
+            // 
+            this.txtHp.Location = new System.Drawing.Point(557, 145);
+            this.txtHp.Name = "txtHp";
+            this.txtHp.Size = new System.Drawing.Size(151, 23);
+            this.txtHp.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(500, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "휴대폰";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(500, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "나이";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(500, 223);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(102, 29);
+            this.btnInsert.TabIndex = 14;
+            this.btnInsert.Text = "추가";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(500, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(102, 29);
+            this.btnSelect.TabIndex = 15;
+            this.btnSelect.Text = "조회";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(608, 223);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(102, 29);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(500, 258);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(102, 29);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(608, 258);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(102, 29);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "리셋";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // nAge
+            // 
+            this.nAge.Location = new System.Drawing.Point(557, 174);
+            this.nAge.Name = "nAge";
+            this.nAge.Size = new System.Drawing.Size(151, 23);
+            this.nAge.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 450);
+            this.Controls.Add(this.nAge);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "데이터 매니저 v3.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtUid;
+        private TextBox txtName;
+        private Label label3;
+        private TextBox txtHp;
+        private Label label4;
+        private Label label5;
+        private Button btnInsert;
+        private Button btnSelect;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnReset;
+        private NumericUpDown nAge;
+    }
+}
