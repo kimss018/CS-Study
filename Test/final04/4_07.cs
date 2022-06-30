3c5076b87a58f4f11edadf14873ec6f0d5837174
kimss018 <kimss018@naver.com>
2022-06-30 17:16:24 +0900

    연습문제

---
+++ Test/final04/4_07.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.final04
+{
+    interface IRunnable
+    {
+        public void Run();
+    }
+
+    interface IFlyable
+    {
+        public void Fly();
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Car Run!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Car Fly!");
+        }
+    }
+
+    internal class _4_07
+    {
+        static void Main7(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+
+            IRunnable runable = car as IRunnable;
+            IFlyable flyable = car as IFlyable;
+
+            runable.Run();
+            flyable.Fly();
+        }
+    }
+}
