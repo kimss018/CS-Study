0f5194e1bceee6fde09fe9aa9f3dc652ece69007
kimss018 <kimss018@naver.com>
2022-07-01 15:33:58 +0900

    교재예제

---
+++ Book/Ch12/P528.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch12
+{
+    internal class P528
+    {
+        static void Main1(string[] args)
+        {
+            List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var output = from item in input
+                         where (item > 5) && (item % 2 == 0)
+                         select item;
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
